ede045019b49626d87c4a2d98f621ef56a61ca71
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-05-21 20:55:39 -0300

    save photo like on local storage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react'
 
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import { Article, ImgWrapper, Img, Button } from './styles'
 
 const DEFAULT_PHOTO = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_PHOTO}) => {
+  const key = `like-${id}`
+
   const ref = useRef(null)
   const [ show, setShow ] = useState(false)
+  const [ liked, setLiked ] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch(err) {
+      return false
+    }
+  })
 
   useEffect(() => {
     Promise.resolve(
@@ -16,7 +26,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_PHOTO}) => {
         : import('intersection-observer') 
 
     ).then( () => {
-      
+
       const observer = new IntersectionObserver( entries => {
         const { isIntersecting } = entries[0]
         if(isIntersecting) {
@@ -29,6 +39,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_PHOTO}) => {
     
   }, [ref])
 
+  const LikedIcon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch(err) {
+      console.log(`Faild save data on local storage: ${err}`)
+    }
+  }
+
   return(
     <Article ref={ref}>
       {
@@ -39,8 +60,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_PHOTO}) => {
        		  <Img src={src} />
        		</ImgWrapper>
           </a>
-          <Button type="button">
-            <MdFavoriteBorder size='32px' /> 
+          <Button type="button" onClick={ () => setLocalStorage(!liked)}>
+            <LikedIcon size='32px' />
             { likes } likes!
           </Button>
         </>
