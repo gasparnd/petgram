3de803f4d7dd4ad02399efd76a31356cffe469e9
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-05-23 08:30:08 -0300

    private routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
+
 import { Logo } from './components/Logo'
 import { GlobalWrapper } from './components/GlobalWrapper'
 import { NavBar } from './components/NavBar'
 
 import { GlobalStyles } from './styles/GlobalStyles'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return(
@@ -20,6 +28,20 @@ export const App = () => {
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
         </Router>
+
+        <UserLogged>
+          {
+            ({ isAuth }) => isAuth 
+              ? <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              : <Router>
+                  <NotRegisterUser path='/favs' />
+                  <NotRegisterUser path='/user' />
+                </Router>
+          }
+        </UserLogged>
   	  </GlobalWrapper>
       <NavBar />
   	</>
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NotRegisterUser = () => {
+  return(
+    <h1>NotRegisterUser</h1>
+  )
+}
