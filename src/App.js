bde354b4999a8d9e86b8702e6b474575074e7a74
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-05-24 12:45:31 -0300

    default route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Router } from '@reach/router'
-import Context from './Context'
+import React, { useContext } from 'react'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisterUser } from './pages/NotRegisterUser'
 
 import { Logo } from './components/Logo'
@@ -19,29 +20,23 @@ const UserLogged = ({ children }) => {
 }
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return(
   	<>
   	  <GlobalStyles />
-  	    <Logo />
-        <Router>
-          <Home path='/' />
-          <Home path='/pet/:id' />
-          <Detail path='/detail/:detailId' />
-        </Router>
-
-        <Context.Consumer>
-          {
-            ({ isAuth }) => isAuth 
-              ? <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-              : <Router>
-                  <NotRegisterUser path='/favs' />
-                  <NotRegisterUser path='/user' />
-                </Router>
-          }
-        </Context.Consumer>
+  	  <Logo />
+      <Router>
+        <NotFound default />
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
+      </Router>
       <NavBar />
   	</>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from '@reach/router'
+
+export const NotFound = () => (
+  <>
+    <h2>Error 404: Pagina no encontrada :(</h2>
+  </>
+)
